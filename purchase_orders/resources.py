7a7e466c8c6a24c4deed15c5feaf8f31eb831325
Adriano Barbosa <adriano.barbosa@infoxnet.com.br>
2025-11-18 14:26:06 -0300

    Novos endpoints utilizando flask restful

--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify
-from flask_restful import Api, Resource
+from flask import jsonify
+from flask_restful import Resource, reqparse
 
 
 purchase_orders = [{
@@ -24,7 +24,33 @@ purchase_orders = [{
 
 
 class PurchaseOrders(Resource):
+  
+  parser = reqparse.RequestParser()
+
+  parser.add_argument('id', type=int, required=True, help="Informe um id")
+  parser.add_argument('description', type=str, required=True, help="Informe uma descricao")
+
   def get(self):
     return jsonify(purchase_orders)
+  
+  def post(self):
+    request_data = PurchaseOrders().parser.parse_args()
+
+    purchase_order = {
+      "id": request_data['id'],
+      "description": request_data["description"],
+      "items": []
+    }
+
+    purchase_orders.append(purchase_order)
+
+    return jsonify(purchase_order)
 
 
+class PurchaseOrdersById(Resource):
+  def get(self, id):
+    for po in purchase_orders:
+      if po['id'] == id:
+        return jsonify(po)
+      
+    return jsonify({'message': f'Pedido {id} não foi encontrado'})
