043d71a0640aee8ad79d5a569c0c144dbbc77a00
Adriano Barbosa <adriano.barbosa@infoxnet.com.br>
2025-11-18 18:20:47 -0300

    Removido os módulos

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
-from purchase_orders_items.resources import PurchaseOrdersItems
 
+#from purchase_orders.resources import PurchaseOrders
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
 
-api.add_resource(PurchaseOrders, '/purchase_orders')
-api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
-api.add_resource(PurchaseOrdersItems, '/purchase_orders/<int:id>/items')
+  app = Flask(__name__)
+  api = Api(app)
 
-app.run(port=5000, debug=True)
+  #api.add_resource(PurchaseOrders, '/purchase_orders')
+
+  return app
