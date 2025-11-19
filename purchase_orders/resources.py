71756034d2a171879e023d1ce901b6f4b3a2585f
Adriano Barbosa <adriano.barbosa@infoxnet.com.br>
2025-11-19 09:24:12 -0300

    Novo test

--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 purchase_orders = [
   {
@@ -18,5 +18,22 @@ purchase_orders = [
 
 class PurchaseOrders(Resource):
 
+  parser = reqparse.RequestParser()
+
+  parser.add_argument('id', type=int, required=True, help='Informe um ID válido')
+  parser.add_argument('description', type=str, required=True, help='Informe uma deescrição válida')
+
   def get(self):
     return jsonify(purchase_orders)
+  
+  def post(self):
+    data = PurchaseOrders.parser.parse_args()
+
+    purchase_order = {
+      'id': data['id'],
+      'description': data['description']
+    }
+
+    purchase_orders.append(purchase_order)
+
+    return purchase_order
--- tests/test_purchase_orders/test_resources.py
+++ tests/test_purchase_orders/test_resources.py
@@ -1,6 +1,21 @@
+import json
+
 def test_get_purchase_orders(test_client):
   response = test_client.get('/purchase_orders')
 
   assert response.status_code == 200
   assert response.json[0]['id'] == 1
   assert response.json[0]['description'] == 'Pedido nro 1'
+
+def test_post_purchase_orders(test_client):
+  obj = {'id': 2, 'description': 'Pedido nro 2'}
+
+  response = test_client.post(
+    '/purchase_orders',
+    data=json.dumps(obj),
+    content_type='application/json'
+  )
+
+  assert response.status_code == 200
+  assert response.json['id'] == obj['id']
+  assert response.json['description'] == obj['description']
